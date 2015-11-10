df5751d9618d29b7fa9cb337e12a08fe6ec9f161
brb7nu <brb7nu@virginia.edu>
2015-11-10 09:59:48 -0500

    Set up timer for 700*32 Hz interrupt. Change to tab indentation

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <msp430.h> 
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
+	// initialize timer
+	DCOCTL = CALDCO_16MHZ;  			// |Set clock speed to 1 MHz|
+	BCSCTL1 = CALBC1_16MHZ;  			// |                        |
+
+	// setup Timer A
+	// SMCLK clock, divided by 8, in up mode, and start by clearing the timer
+	TACTL = TASSEL_2 | ID_3 | MC_1 | TACLR;
+
+	// setup value for comparison
+	// Create a timer interrupt which occurs at (700*32) Hz
+	// 16 MHz / 8 = 2 MHz --> 90 ticks per 45 us
+	TACCR0 = 90;
+
+	// enable interrupt on capture-compare control register 0
+	TACCTL0 |= CCIE;
+
 	return 0;
 }
